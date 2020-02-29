d999582cc10e57cbf49900ee22ac995708fbea2f
Michal <michal@jedryszka.pl>
2020-02-29 12:22:50 +0100

    List of departments

--- src/main/java/pl/sda/java/web/employee/servlet/DepartmentsServlet.java
+++ src/main/java/pl/sda/java/web/employee/servlet/DepartmentsServlet.java
@@ -1,17 +1,26 @@
 package pl.sda.java.web.employee.servlet;
 
 
+import pl.sda.java.web.employee.model.Department;
+import pl.sda.java.web.employee.service.DepartmentService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(urlPatterns="/departments")
+@WebServlet(name="departments", urlPatterns="/departments")
 public class DepartmentsServlet extends HttpServlet {
+
+    private DepartmentService departmentService = new DepartmentService();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Department> departments = departmentService.getAllDepartments();
+        req.setAttribute("departments", departments);
         req.getRequestDispatcher("/WEB-INF/jsp/departments.jsp")
                 .forward(req, resp);
     }
--- src/main/webapp/WEB-INF/jsp/departments.jsp
+++ src/main/webapp/WEB-INF/jsp/departments.jsp
@@ -1,1 +1,8 @@
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<form>
+    
+</form>
 Departments:
+<c:forEach items="${departments}" var = "dep">
+    <p><c:out value = "${dep.deptNo}"/> <c:out value = "${dep.deptName}"/></p>
+</c:forEach>
