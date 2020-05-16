75cc1420b9174cc71645f474793a661844b0d0b3
Michal <michal@jedryszka.pl>
2020-05-16 12:28:01 +0200

    Clean empty version

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,11 +4,6 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.query.Query;
-import pl.sda.java.web.employee.model.Department;
-import pl.sda.java.web.employee.model.Employee;
-
-import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
@@ -16,16 +11,6 @@ public class Runner {
         try(SessionFactory sessionFactory = new MetadataSources(standardServiceRegistry).buildMetadata().buildSessionFactory()) {
             Session session = sessionFactory.openSession();
             Transaction transaction = session.beginTransaction();
-
-            Query<Department> query = session.createQuery("SELECT d from Department d", Department.class);
-            List<Department> resultList = query.getResultList();
-            System.out.println(resultList);
-
-            Query<Employee> queryEmployee = session.createQuery(
-                    "SELECT e from Employee e", Employee.class);
-            List<Employee> employeeList = queryEmployee.getResultList();
-            System.out.println(employeeList.size());
-
             transaction.commit();
             session.close();
         } catch (Exception e) {
--- src/main/java/pl/sda/java/web/employee/service/Operation.java
+++
@@ -1,7 +0,0 @@
-package pl.sda.java.web.employee.service;
-
-import org.hibernate.Session;
-
-interface Operation<T> {
-    T execute(Session session);
-}
