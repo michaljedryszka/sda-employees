8ba641d796422a39ddb6c94bee2bb35b8df32f61
Michal <michal@jedryszka.pl>
2020-05-23 12:54:54 +0200

    Remaining Employee attributes save

--- src/main/java/pl/sda/java/web/employee/servlet/EmployeeServlet.java
+++ src/main/java/pl/sda/java/web/employee/servlet/EmployeeServlet.java
@@ -1,6 +1,7 @@
 package pl.sda.java.web.employee.servlet;
 
 import pl.sda.java.web.employee.model.Employee;
+import pl.sda.java.web.employee.model.Gender;
 import pl.sda.java.web.employee.service.EmployeeService;
 
 import javax.servlet.ServletException;
@@ -9,11 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "employee", urlPatterns = "/employee")
 public class EmployeeServlet extends HttpServlet {
 
     private EmployeeService employeeService = new EmployeeService();
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,8 +34,21 @@ public class EmployeeServlet extends HttpServlet {
         Employee employee = employeeService.getByEmployeeNumber(Integer.valueOf(id));
         employee.setFirstName(req.getParameter("firstName"));
         employee.setLastName(req.getParameter("lastName"));
+
+        String gender = req.getParameter("gender");
+        employee.setGender(Gender.valueOf(gender));
+
+        LocalDate birthDate = getDate(req.getParameter("birthDate"));
+        employee.setBirthDate(birthDate);
+
+        employee.setHireDate(getDate(req.getParameter("hireDate")));
+
         employeeService.save(employee);
         //after post redirect
         resp.sendRedirect("employee?id=" + id);
+    }
+
+    private LocalDate getDate(String date) {
+        return LocalDate.parse(date, formatter);
     }
 }
